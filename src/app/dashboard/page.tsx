db8704a2c4fde50c40da4f213ba10c58f2064905
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-03 20:27:41 +1000

    feat: add page routes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <main className="text-center pt-8 px-5 flex flex-col items-center gap-y-4">
+      Dashboard Page
+    </main>
+  )
+}
---
+++ src/app/kanban/page.tsx
@@ -0,0 +1,7 @@
+export default function Kanban() {
+  return (
+    <main className="text-center pt-8 px-5 flex flex-col items-center gap-y-4">
+      Kanban Page
+    </main>
+  )
+}
