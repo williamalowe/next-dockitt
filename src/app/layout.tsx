bac9e0a6c66ffb0ee29ee3bce88d087b8a48788f
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-03 20:26:15 +1000

    fix: relocate components folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Container from "./components/container";
-import Header from "./components/header";
+import Container from "../components/container";
+import Header from "../components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
