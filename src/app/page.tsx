a12d7f5daca85145b4684a5b00e5d6b8e4b1de8e
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-05 14:10:10 +1000

    feat: add home-page task counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
   return (
     <main className="pt-8 px-5 flex flex-col">
       <h1 className="text-2xl font-bold">Welcome Back!</h1>
-      <p>You have x tasks pending.</p>
+      <p>You have <strong>{tasks.length}</strong> tasks pending.</p>
       <TaskList 
         tasks={tasks}
       />
