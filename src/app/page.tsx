30b5eab5227cc3f7f1139ab6d812f8dec52c00e7
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-11 14:19:25 +1000

    feat: add task-list component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-export default async function Home() {
+import TaskList from "@/components/task-list";
+
+export default function Home() {
 
   return (
-    <main className="pt-8 px-5 flex flex-col">
-      hello world
+    <main className="flex-1 pt-8 px-5 flex flex-col gap-y-4">
+      <h1 className="text-2xl font-bold">All Tasks <span className="ml-8 font-normal">0</span></h1>
+      <TaskList />
     </main>
   );
 }
---
+++ src/components/task-list.tsx
@@ -0,0 +1,47 @@
+import { exampleTasks } from "@/lib/testData";
+import { BsCaretLeft, BsCaretRight, BsX } from "react-icons/bs";
+
+export default function TaskList() {
+  return (
+    <div className="flex flex-col border w-full rounded-md p-2">
+      <div className="flex">
+        <div className="flex-1 uppercase font-bold">Task</div>
+        <div className="flex-1 uppercase font-bold">Priority</div>
+        <div className="flex-1 uppercase font-bold">Status</div>
+        <div className="flex-1"></div>
+      </div>
+      {exampleTasks.map((task) => (
+        <div key={task.id} className="flex">
+          <div className="flex-1 capitalize">{task.task}</div>
+          <div className="flex-1 capitalize">{task.priority}</div>
+          <div
+            className={`${
+              task.status === "backlog"
+                ? "bg-rose-800"
+                : task.status === "in progress"
+                ? "bg-orange-800"
+                : task.status === "under review"
+                ? "bg-purple-800"
+                : task.status === "completed"
+                ? "bg-green-800"
+                : "bg-red-800"
+            } flex-1 capitalize rounded-md`}
+          >
+            {task.status}
+          </div>
+          <div className="flex-1 flex justify-end">
+            <button>
+              <BsX />
+            </button>
+            <button>
+              <BsCaretLeft />
+            </button>
+            <button>
+              <BsCaretRight />
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
