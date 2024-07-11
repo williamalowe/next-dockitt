b268a6b8d00986d3fa94e7a78506128a185fba77
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-11 14:46:24 +1000

    func: add sidebar component task status counts

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -8,6 +8,7 @@ import {
   BsMenuButton,
   BsMenuDown,
 } from "react-icons/bs";
+import { exampleTasks } from "@/lib/testData";
 
 export default function Sidebar() {
   const listLinks = [
@@ -33,35 +34,35 @@ export default function Sidebar() {
       title: "Backlog",
       color: "bg-rose-800",
       textColor: "text-rose-800",
-      count: 0,
+      count: exampleTasks.filter((task) => task.status === "backlog").length,
     },
     {
       href: "/in-progress",
       title: "In Progress",
       color: "bg-orange-800",
       textColor: "text-orange-800",
-      count: 0,
+      count: exampleTasks.filter((task) => task.status === "in progress").length
     },
     {
       href: "/under-review",
       title: "Under Review",
       color: "bg-purple-800",
       textColor: "text-purple-800",
-      count: 0,
+      count: exampleTasks.filter((task) => task.status === "under review").length
     },
     {
       href: "/completed",
       title: "Completed",
       color: "bg-green-800",
       textColor: "text-green-800",
-      count: 0,
+      count: exampleTasks.filter((task) => task.status === "completed").length
     },
     {
       href: "/cancelled",
       title: "Cancelled",
       color: "bg-red-800",
       textColor: "text-red-800",
-      count: 0,
+      count: exampleTasks.filter((task) => task.status === "cancelled").length
     },
   ];
   return (
