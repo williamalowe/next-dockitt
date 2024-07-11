55c7804bef4a987710f321aa7b8528221f5b946f
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-11 12:12:15 +1000

    refactor/ remove old project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,8 @@
-import dbConnect from "@/lib/db";
-import { getTasks } from "@/actions/action";
-import TaskList from "@/components/task-list";
-import Image from "next/image";
-
 export default async function Home() {
-  await dbConnect();
-  const tasks = await getTasks();
 
   return (
     <main className="pt-8 px-5 flex flex-col">
-      <h1 className="text-2xl font-bold">Welcome Back!</h1>
-      <p>You have <strong>{tasks.length}</strong> tasks pending.</p>
-      <TaskList 
-        tasks={tasks}
-      />
+
     </main>
   );
 }
--- src/components/container.tsx
+++
@@ -1,7 +0,0 @@
-export default function Container({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="max-w-[1100px] mx-auto bg-neutral-900 min-h-screen flex flex-col text-white">
-      {children}
-    </div>
-  );
-}
