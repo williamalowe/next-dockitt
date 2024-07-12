bfd8d0b2a6368478276743ef2104fa65df1c3e03
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-12 12:56:44 +1000

    feat: add sidebar hiding option

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Link from "next/link";
 import {
   BsGraphDown,
@@ -6,13 +6,18 @@ import {
   BsKanban,
   BsListCheck,
   BsPlus,
+  BsX,
 } from "react-icons/bs";
 import { useTask } from "@/context/task-context";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Sidebar() {
   const { tasks } = useTask();
   const pathname = usePathname();
+  const [hidden, setHidden] = useState(false);
+
   const listLinks = [
     {
       href: "/",
@@ -48,16 +53,14 @@ export default function Sidebar() {
       title: "In Progress",
       color: "bg-orange-800",
       textColor: "text-orange-800",
-      count: tasks.filter((task) => task.status === "in progress")
-        .length,
+      count: tasks.filter((task) => task.status === "in progress").length,
     },
     {
       href: "/under-review",
       title: "Under Review",
       color: "bg-purple-800",
       textColor: "text-purple-800",
-      count: tasks.filter((task) => task.status === "under review")
-        .length,
+      count: tasks.filter((task) => task.status === "under review").length,
     },
     {
       href: "/completed",
@@ -75,59 +78,105 @@ export default function Sidebar() {
     },
   ];
   return (
-    <div
-      className="m-2 bg-zinc-100/80 p-2 rounded-md w-[240px] flex flex-col gap-y-4 dark: 
-    dark:bg-zinc-800/80"
-    >
-      <div className="flex items-center">
-        <Link href="/">
-          <h5 className="font-bold text-lg p-2 border rounded-md bg-zinc-900 text-zinc-50">
-            dockitt.
-          </h5>
-        </Link>
-        <button className="font-bold ml-auto opacity-80">
+    <>
+      <AnimatePresence mode="popLayout">
+        {!hidden && (
+          <motion.div
+            className="absolute md:static top-0 left-0 z-[999] w-screen md:w-[240px] h-full bg-zinc-100 p-2 rounded-md  flex flex-col gap-y-4 dark: 
+      dark:bg-zinc-800"
+            initial={{
+              opacity: 0,
+              x: -268,
+            }}
+            animate={{
+              opacity: 1,
+              x: 0,
+            }}
+            exit={{
+              opacity: 0,
+              x: -268,
+            }}
+            transition={{
+              type: "tween",
+            }}
+          >
+            <div className="flex items-center">
+              <Link href="/">
+                <h5 className="font-bold text-lg p-2 border rounded-md bg-zinc-900 text-zinc-50">
+                  dockitt.
+                </h5>
+              </Link>
+              <button
+                onClick={() => setHidden(!hidden)}
+                className="flex items-center justify-center font-bold ml-auto opacity-80 text-xl w-6 h-6 
+                            rounded hover:bg-zinc-300 dark:hover:bg-zinc-900 transition"
+              >
+                <BsX />
+              </button>
+            </div>
+            <div>
+              <h5 className="font-bold text-sm opacity-80">Views</h5>
+              <ul className="mt-2 flex flex-col gap-y-2">
+                {listLinks.map((link) => (
+                  <li
+                    key={link.href}
+                    className={`${
+                      pathname === link.href
+                        ? "opacity-100 font-bold bg-zinc-300 dark:bg-zinc-900"
+                        : "opacity-80"
+                    } px-2 py-1 text-sm rounded-md hover:bg-zinc-300 
+                    dark:hover:bg-zinc-900 transition`}
+                  >
+                    <Link
+                      href={link.href}
+                      className="flex items-center justify-between"
+                    >
+                      {link.icon} {link.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h5 className="font-bold text-sm opacity-80">Task List</h5>
+              <ul className="mt-2 flex flex-col gap-y-2">
+                {taskLists.map((link) => (
+                  <li
+                    key={link.href}
+                    className={`${
+                      pathname === link.href
+                        ? "opacity-100 font-bold bg-zinc-300 dark:bg-zinc-900"
+                        : "opacity-80"
+                    } px-2 py-1 opacity-80 text-sm rounded-md hover:bg-zinc-300 
+                   dark:hover:bg-zinc-900 
+                transition`}
+                  >
+                    <Link
+                      href={link.href}
+                      className="flex items-center gap-x-2"
+                    >
+                      <div className={`${link.color} h-4 w-4 rounded-md`}></div>{" "}
+                      {link.title}{" "}
+                      <span className={`${link.textColor} ml-auto`}>
+                        {link.count}
+                      </span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      {hidden && (
+        <button
+          onClick={() => setHidden(!hidden)}
+          className="absolute top-1 left-1 flex items-center justify-center font-bold ml-auto opacity-80 text-xl w-6 h-6 
+                            rounded hover:bg-zinc-300 dark:hover:bg-zinc-900 transition"
+        >
           <BsGrid />
         </button>
-      </div>
-      <div>
-        <h5 className="font-bold text-sm opacity-80">Views</h5>
-        <ul className="mt-2 flex flex-col gap-y-2">
-          {listLinks.map((link) => (
-            <li
-              key={link.href}
-              className={`${pathname === link.href ? "opacity-100 font-bold bg-zinc-300 dark:bg-zinc-900" : "opacity-80"} px-2 py-1 text-sm rounded-md hover:bg-zinc-300 
-              dark:hover:bg-zinc-900 transition`}
-            >
-              <Link
-                href={link.href}
-                className="flex items-center justify-between"
-              >
-                {link.icon} {link.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <h5 className="font-bold text-sm opacity-80">Task List</h5>
-        <ul className="mt-2 flex flex-col gap-y-2">
-          {taskLists.map((link) => (
-            <li
-              key={link.href}
-              className={`${pathname === link.href ? "opacity-100 font-bold bg-zinc-300 dark:bg-zinc-900" : "opacity-80"} px-2 py-1 opacity-80 text-sm rounded-md hover:bg-zinc-300 dark:hover:bg-zinc-900 
-              transition`}
-            >
-              <Link href={link.href} className="flex items-center gap-x-2">
-                <div className={`${link.color} h-4 w-4 rounded-md`}></div>{" "}
-                {link.title}{" "}
-                <span className={`${link.textColor} ml-auto`}>
-                  {link.count}
-                </span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
