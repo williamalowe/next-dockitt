d19c4b0ca5b1ff9b0fc8c9a3bc4c53a99915b36a
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-29 13:58:04 +1000

    feat: add new-task-dropdown component to kanban-column component

--- src/components/kanban-column.tsx
+++ src/components/kanban-column.tsx
@@ -1,18 +1,25 @@
-import KanbanCard from "./kanban-card"
+import KanbanCard from "./kanban-card";
+import NewTaskDropdown from "./new-task-dropdown";
 
-export default function KanbanColumn({ header, textColor, taskList, cardColor }: {
-  header: string
-  textColor: string,
-  taskList: any,
-  cardColor: string
+export default function KanbanColumn({
+  header,
+  textColor,
+  taskList,
+  cardColor,
+}: {
+  header: string;
+  textColor: string;
+  taskList: any;
+  cardColor: string;
 }) {
   return (
     <div className="flex-1">
-      <h5 className={`${textColor} uppercase italic text-xl`}>{header} - {taskList.length}</h5>
+      <h5 className={`${textColor} uppercase italic text-xl`}>
+        {header} - {taskList.length}
+      </h5>
       <div className="flex p-1 gap-2 md:flex-col flex-wrap">
-        {
-          taskList.map((task) => 
-          <KanbanCard 
+        {taskList.map((task) => (
+          <KanbanCard
             key={task.id}
             color={cardColor}
             id={task.id}
@@ -22,13 +29,12 @@ export default function KanbanColumn({ header, textColor, taskList, cardColor }:
             priority={task.priority}
             status={task.status}
           />
-          )
-        }
-        {
-          taskList.length === 0 && 
-          <p className="italic">No Tasks</p>
-        }
+        ))}
+        {taskList.length === 0 && <p className="italic">No Tasks</p>}
+        {header !== "Cancelled" && (
+          <NewTaskDropdown status={header.toLocaleLowerCase()} />
+        )}
       </div>
     </div>
-  )
+  );
 }
