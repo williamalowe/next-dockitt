ac255aae3b29c3123a34b35d80d6049e45518395
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-29 13:47:08 +1000

    refactor: replace newTaskForm with NewTaskDropdown on [status] page

--- src/app/[status]/page.tsx
+++ src/app/[status]/page.tsx
@@ -1,8 +1,7 @@
-"use client"
+"use client";
+import NewTaskDropdown from "@/components/new-task-dropdown";
 import TaskList from "@/components/task-list";
 import { useTask } from "@/context/task-context";
-import Link from "next/link";
-import { BsPlus } from "react-icons/bs";
 
 export default function Status({
   params,
@@ -12,17 +11,13 @@ export default function Status({
   };
 }) {
   const { tasks } = useTask();
-  let displayTasks = tasks.filter(
-    (task) => task.status === params.status
-  );
+  let displayTasks = tasks.filter((task) => task.status === params.status);
 
   if (params.status === "in-progress") {
     displayTasks = tasks.filter((task) => task.status === "in progress");
   }
   if (params.status === "under-review") {
-    displayTasks = tasks.filter(
-      (task) => task.status === "under review"
-    );
+    displayTasks = tasks.filter((task) => task.status === "under review");
   }
 
   return (
@@ -32,13 +27,16 @@ export default function Status({
         <span className="ml-8 font-normal">{displayTasks.length}</span>
       </h1>
       <TaskList tasks={displayTasks} />
-      <Link
+      {/* <Link
         // href="/new-task"
         href={`/${params.status}/new-task`}
         className="flex items-center justify-center bg-zinc-900 text-zinc-50/80 p-1 text-xl rounded-md dark:bg-zinc-200 dark:text-zinc-900/80 hover:scale-[1.025] transition"
       >
         <BsPlus />
-      </Link>
+      </Link> */}
+      {params.status !== "cancelled" && (
+        <NewTaskDropdown status={params.status} />
+      )}
     </main>
   );
 }
