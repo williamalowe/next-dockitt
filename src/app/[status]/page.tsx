7a2ad4d28c30388b1d23cc302efeabf8f743cc5a
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-29 14:06:46 +1000

    refactor: remove unused [status] @modal route

--- src/app/[status]/page.tsx
+++ src/app/[status]/page.tsx
@@ -27,13 +27,6 @@ export default function Status({
         <span className="ml-8 font-normal">{displayTasks.length}</span>
       </h1>
       <TaskList tasks={displayTasks} />
-      {/* <Link
-        // href="/new-task"
-        href={`/${params.status}/new-task`}
-        className="flex items-center justify-center bg-zinc-900 text-zinc-50/80 p-1 text-xl rounded-md dark:bg-zinc-200 dark:text-zinc-900/80 hover:scale-[1.025] transition"
-      >
-        <BsPlus />
-      </Link> */}
       {params.status !== "cancelled" && (
         <NewTaskDropdown
           status={
